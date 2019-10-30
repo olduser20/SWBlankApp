004661a9e767a739f8c5b6ea022007be90e782b5
Saber Nemati <m.saber.n@gmail.com>
2019-10-30 18:23:12 +0330

    Minor changes

--- SWBlankApp/Program.cs
+++ SWBlankApp/Program.cs
@@ -5,20 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using SolidWorks.Interop.sldworks;
 using SolidWorks.Interop.swconst;
+using Microsoft.Office.Interop;
 //using SolidWorks.Interop.swpublished;
 
 namespace SWBlankApp
 {
     class Program
     {
+
+        //Microsoft.Office.Interop.MSProject
+
+        
         //string[] args
-        static void Main()
+        static void CreateSketch()
         {
             SldWorks swApp = new SldWorks();
             //Feature myFeature;
-            ModelDoc2 swModel =default(ModelDoc2);
+            ModelDoc2 swModel = default(ModelDoc2);
             //RefPlane myRefPlane;
-            SketchManager swSkMgr=default(SketchManager);
+            SketchManager swSkMgr = default(SketchManager);
             FeatureManager swFeMgr = default(FeatureManager);
 
             //Feature swFeat;
@@ -28,7 +33,7 @@ namespace SWBlankApp
 
             swApp.ResetUntitledCount(0, 0, 0);
 
-            swModel = (ModelDoc2)swApp.NewDocument("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\PartTemplate.SLDPRT", 0,0,0);
+            swModel = (ModelDoc2)swApp.NewDocument("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\PartTemplate.SLDPRT", 0, 0, 0);
             swApp.ActivateDoc2("Part1", false, ref longstatus);
             swModel = (ModelDoc2)swApp.ActiveDoc;
 
@@ -39,11 +44,11 @@ namespace SWBlankApp
             //swFeMgr.FeatureExtruRefSurface()
             boolstatus = swModel.Extension.SelectByID2("Top Plane", "PLANE", -0.0553489443349025, 0.00330468607538553, 0.0269617286188933, false, 0, null, 0);
             swModel.ClearSelection2(true);
-            
+
 
             if (swModel == null)
             {
-                swApp.SendMsgToUser2("Ridi.",(int)swMessageBoxIcon_e.swMbWarning ,(int)swMessageBoxBtn_e.swMbOk);
+                swApp.SendMsgToUser2("Ridi.", (int)swMessageBoxIcon_e.swMbWarning, (int)swMessageBoxBtn_e.swMbOk);
                 return;
             }
 
@@ -74,11 +79,8 @@ namespace SWBlankApp
             swSkMgr.InsertSketch(true);
 
 
-            Feature feRevolve = default(Feature);
             
-            feRevolve = swFeMgr.FeatureRevolve(90, false, 0, 0, 0, false, false, false);
 
-            
             //swModel.SketchCircle();
 
             Console.WriteLine(swApp.Visible.ToString());
@@ -87,8 +89,8 @@ namespace SWBlankApp
 
             //swApp.CreateNewWindow();
 
-            swModel.SaveAs("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\PartModefied.SLDPRT");
-            
+            swModel.SaveAs("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\SketchArc.SLDPRT");
+
 
             //swApp.
 
@@ -97,7 +99,56 @@ namespace SWBlankApp
             //swApp = null;
 
             //swApp
+        }
 
+        static void CreateRevolve()
+        {
+            SldWorks swApp = new SldWorks();
+            ModelDoc2 swModel;
+            ModelDocExtension swModelDocExt;
+            FeatureManager swFeatMgr;
+            SelectionMgr swSelMgr;
+
+            bool boolstatus;
+
+            swModel = (ModelDoc2)swApp.NewDocument("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\PartTemplate.SLDPRT", 0, 0, 0);
+            //swModel = (ModelDoc2)swApp.ActiveDoc;
+            swModelDocExt = swModel.Extension;
+            swSelMgr = (SelectionMgr)swModel.SelectionManager;
+
+            boolstatus = swModelDocExt.SelectByID2("Right Plane", "PLANE", 0, 0, 0, true, 0, null, (int)swSelectOption_e.swSelectOptionDefault);
+            boolstatus = swModelDocExt.SelectByID2("Top Plane", "PLANE", 0, 0, 0, true, 0, null, (int)swSelectOption_e.swSelectOptionDefault);
+            swModel.InsertAxis2(true);
+
+
+            boolstatus = swModelDocExt.SelectByID2("Top Plane", "PLANE", -0.08954836342753, 0.0004336873289503, 0.006720765739942, false, 0, null, (int)swSelectOption_e.swSelectOptionDefault);
+            swModel.InsertSketch2(true);
+            swModel.ClearSelection2(true);
+            swModel.SketchRectangle(-0.05, -0.05, 0, 0, 0.1, 0, true);
+
+            swModel.InsertSketch2(true);
+            swModel.ShowNamedView2("*Trimetric", 8);
+
+            boolstatus = swModelDocExt.SelectByID2("Sketch1", "SKETCH", 0, 0, 0, false, 0, null, (int)swSelectOption_e.swSelectOptionDefault);
+            boolstatus = swModelDocExt.SelectByID2("Axis1", "AXIS", 0, 0, 0, true, 4, null, (int)swSelectOption_e.swSelectOptionDefault);
+            swFeatMgr = swModel.FeatureManager;
+
+            swFeatMgr.FeatureRevolve2(true, true, false, false, false, false, 0, 0, 6.28318530718, 0, false,
+            false, 0.01, 0.01, 0, 0, 0, true, true, true);
+
+
+            swModel.SaveAs("C:\\Users\\nemati\\Documents\\Visual Studio 2015\\Projects\\SWBlankApp\\SWBlankApp\\FeatureRevolve.SLDPRT");
+
+            swApp.ExitApp();
+
+
+        }
+
+        static void Main()
+        {
+
+            CreateSketch();
+            CreateRevolve();
             
 
         }
